14070acfb749146ffa5ea67784f18f24f52d6564
Raku Watari <rwatari@gmail.com>
2016-11-21 16:27:38 -0800

    Create sliding_piece module

--- piece.rb
+++ piece.rb
@@ -1,3 +1,12 @@
 class Piece
-  
+  attr_reader :color, :pos, :board
+  def initialize(color, pos, board)
+    @color = color
+    @pos = pos
+    @board = board
+  end
+
+  def same_team?(other_piece)
+    @color == other_piece.color
+  end
 end
---
+++ sliding_piece.rb
@@ -0,0 +1,45 @@
+require_relative 'piece'
+
+module Slideable
+  DIAGONALS = [
+    [ 1,  1],
+    [ 1, -1],
+    [-1,  1],
+    [-1, -1]
+  ]
+
+  STRAIGHTS = [
+    [ 0,  1],
+    [ 0, -1],
+    [ 1,  0],
+    [-1,  0]
+  ]
+
+  def moves
+    poss_moves = []
+    move_dirs.each do |dir|
+      curr_pos = update_pos(pos, dir)
+      while self.board.in_bounds?(curr_pos)
+        if self.board[curr_pos] == nil
+          poss_moves << curr_pos
+        elsif self.board[curr_pos].same_team?(self)
+          break
+        else
+          #if opposing piece
+          poss_moves << curr_pos
+          break
+        end
+        curr_pos = update_pos(curr_pos, dir)
+
+      end
+    end
+
+    poss_moves
+  end
+
+  def update_pos(position, diff)
+    diff_x, diff_y = diff
+    x, y = position
+    [x + diff_x, y + diff_y]
+  end
+end
